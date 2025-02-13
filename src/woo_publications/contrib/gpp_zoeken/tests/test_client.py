fe636191054ae24c7d07c5e6612ce1673b6cde56
bart-maykin <bart.burgman@maykinmedia.nl>
2025-02-13 11:50:34 +0100

    :white_check_mark: [#150] added test for indexing publication (task and api)

--- src/woo_publications/contrib/gpp_zoeken/tests/test_client.py
+++ src/woo_publications/contrib/gpp_zoeken/tests/test_client.py
@@ -2,7 +2,10 @@ from django.test import TestCase
 
 from woo_publications.contrib.tests.factories import ServiceFactory
 from woo_publications.publications.constants import PublicationStatusOptions
-from woo_publications.publications.tests.factories import DocumentFactory
+from woo_publications.publications.tests.factories import (
+    DocumentFactory,
+    PublicationFactory,
+)
 from woo_publications.utils.tests.vcr import VCRMixin
 
 from ..client import get_client
@@ -33,6 +36,35 @@ class SearchClientTests(VCRMixin, TestCase):
 
         with get_client(service) as client:
             task_id = client.index_document(doc)
+
+        self.assertIsNotNone(task_id)
+        self.assertIsInstance(task_id, str)
+        self.assertNotEqual(task_id, "")
+
+    def test_index_unpublished_publication(self):
+        service = ServiceFactory.build(for_gpp_search_docker_compose=True)
+        client = get_client(service)
+
+        for publication_status in PublicationStatusOptions:
+            if publication_status == PublicationStatusOptions.published:
+                continue
+
+            publication = PublicationFactory.create(publicatiestatus=publication_status)
+            with (
+                self.subTest(publication_status=publication_status),
+                client,
+                self.assertRaises(ValueError),
+            ):
+                client.index_publication(publication)
+
+    def test_index_published_publication(self):
+        service = ServiceFactory.build(for_gpp_search_docker_compose=True)
+        publication = PublicationFactory.create(
+            publicatiestatus=PublicationStatusOptions.published
+        )
+
+        with get_client(service) as client:
+            task_id = client.index_publication(publication)
 
         self.assertIsNotNone(task_id)
         self.assertIsInstance(task_id, str)
