d59d52a2dec071dc0a995cb6371296b75050a107
bart-maykin <bart.burgman@maykinmedia.nl>
2025-06-10 15:31:20 +0200

    :white_check_mark: [#194] added admin tests for the new document identifier inline admin

--- src/woo_publications/publications/tests/test_identifier_admin.py
+++ src/woo_publications/publications/tests/test_identifier_admin.py
@@ -5,8 +5,13 @@ from maykin_2fa.test import disable_admin_mfa
 
 from woo_publications.accounts.tests.factories import UserFactory
 from woo_publications.metadata.tests.factories import InformationCategoryFactory
-from woo_publications.publications.models import PublicationIdentifier
+from woo_publications.publications.models import (
+    DocumentIdentifier,
+    PublicationIdentifier,
+)
 from woo_publications.publications.tests.factories import (
+    DocumentFactory,
+    DocumentIdentifierFactory,
     PublicationFactory,
     PublicationIdentifierFactory,
 )
@@ -126,4 +131,114 @@ class TestInlinePublicationIdentifierForPublicationAdmin(WebTest):
 
         self.assertFalse(
             PublicationIdentifier.objects.filter(pk=publicationidentifier.id).exists()
+        )
+
+
+@disable_admin_mfa()
+class TestInlineDocumentIdentifierForPublicationAdmin(WebTest):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.user = UserFactory.create(superuser=True)
+
+    def test_inline_documentidentifier_admin_create(self):
+        document = DocumentFactory.create(
+            officiele_titel="title one",
+        )
+        reverse_url = reverse(
+            "admin:publications_document_change",
+            kwargs={"object_id": document.id},
+        )
+
+        response = self.app.get(reverse_url, user=self.user)
+
+        self.assertEqual(response.status_code, 200)
+
+        form = response.forms["document_form"]
+        form["documentidentifier_set-TOTAL_FORMS"] = (
+            "1"  # we're adding one, dynamically
+        )
+        add_dynamic_field(form, "documentidentifier_set-0-kenmerk", "OZ-123")
+        add_dynamic_field(
+            form,
+            "documentidentifier_set-0-bron",
+            "documents api",
+        )
+
+        create_response = form.submit(name="_save")
+
+        self.assertEqual(create_response.status_code, 302)
+
+        added_item = DocumentIdentifier.objects.get()
+
+        self.assertEqual(added_item.document, document)
+        self.assertEqual(added_item.kenmerk, "OZ-123")
+        self.assertEqual(added_item.bron, "documents api")
+
+    def test_inline_documentidentifier_admin_update(self):
+        document = DocumentFactory.create(
+            officiele_titel="title one",
+        )
+        documentidentifier = DocumentIdentifierFactory.create(
+            document=document,
+            kenmerk="gpp-zoeken",
+            bron="search api",
+        )
+        reverse_url = reverse(
+            "admin:publications_document_change",
+            kwargs={"object_id": document.id},
+        )
+
+        response = self.app.get(reverse_url, user=self.user)
+
+        self.assertEqual(response.status_code, 200)
+
+        form = response.forms["document_form"]
+        add_dynamic_field(form, "documentidentifier_set-0-kenmerk", "OZ-123")
+        add_dynamic_field(
+            form,
+            "documentidentifier_set-0-bron",
+            "documents api",
+        )
+
+        update_response = form.submit(name="_save")
+
+        self.assertEqual(update_response.status_code, 302)
+
+        documentidentifier.refresh_from_db()
+
+        self.assertEqual(documentidentifier.document, document)
+        self.assertEqual(documentidentifier.kenmerk, "OZ-123")
+        self.assertEqual(
+            documentidentifier.bron,
+            "documents api",
+        )
+
+    def test_inline_documentidentifier_admin_delete(self):
+        document = DocumentFactory.create(
+            officiele_titel="title one",
+        )
+        documentidentifier = DocumentIdentifierFactory.create(
+            document=document,
+            kenmerk="gpp-zoeken",
+            bron="search api",
+        )
+        reverse_url = reverse(
+            "admin:publications_document_change",
+            kwargs={"object_id": document.id},
+        )
+
+        response = self.app.get(reverse_url, user=self.user)
+
+        self.assertEqual(response.status_code, 200)
+
+        form = response.forms["document_form"]
+        form["documentidentifier_set-0-DELETE"] = True
+
+        delete_response = form.submit(name="_save")
+
+        self.assertEqual(delete_response.status_code, 302)
+
+        self.assertFalse(
+            DocumentIdentifier.objects.filter(pk=documentidentifier.id).exists()
         )
