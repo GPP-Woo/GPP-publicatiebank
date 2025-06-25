12bdc1a32274dd1066bafa6bf8dacdae438431d9
Sergei Maertens <sergei@maykinmedia.nl>
2025-06-25 17:44:03 +0200

    :sparkles: [#274] Add extra fields and constants for metadata
    Added extra field in document metadata to keep track of the provided
    documentUrl. The field is readonly, as it is intended to only be
    set via the API and not the admin interface.

--- src/woo_publications/publications/admin.py
+++ src/woo_publications/publications/admin.py
@@ -321,6 +321,7 @@ class DocumentInlineAdmin(admin.StackedInline):
         "laatst_gewijzigd_datum",
         "gepubliceerd_op",
         "ingetrokken_op",
+        "source_url",
     )
     extra = 0
 
@@ -583,6 +584,7 @@ class DocumentAdmin(AdminAuditLogMixin, admin.ModelAdmin):
                     "laatst_gewijzigd_datum",
                     "eigenaar",
                     "uuid",
+                    "source_url",
                 )
             },
         ),
@@ -618,6 +620,7 @@ class DocumentAdmin(AdminAuditLogMixin, admin.ModelAdmin):
         "ingetrokken_op",
         "at_time",
         "was_assciated_with",
+        "source_url",
     )
     search_fields = (
         "uuid",
