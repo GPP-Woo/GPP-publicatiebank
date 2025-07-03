7b6bd5120b5a2b34044522cd8c8df3c26be6d27a
Sergei Maertens <sergei@maykinmedia.nl>
2025-07-03 12:10:44 +0200

    tests

--- src/woo_publications/publications/tests/test_process_source_document_task.py
+++ src/woo_publications/publications/tests/test_process_source_document_task.py
@@ -7,7 +7,7 @@ from django.core.files import File
 from django.test import TestCase
 
 from woo_publications.config.models import GlobalConfiguration
-from woo_publications.contrib.documents_api.client import get_client
+from woo_publications.contrib.documents_api.client import DocumentenClient, get_client
 from woo_publications.contrib.tests.factories import ServiceFactory
 from woo_publications.metadata.tests.factories import (
     InformationCategoryFactory,
@@ -23,6 +23,36 @@ DOCUMENT_TYPE_URL = (
     "http://host.docker.internal:8000/catalogi/api/v1/informatieobjecttypen/"
     + DOCUMENT_TYPE_UUID
 )
+
+
+def _create_initial_document(
+    *,
+    client: DocumentenClient,
+    creation_date: date,
+    content: bytes,
+    filename: str,
+    content_type: str = "text/plain",
+):
+    oz_document = client.create_document(
+        # must be unique for the source organisation
+        identification=str(uuid4()),
+        source_organisation="123456782",
+        document_type_url=DOCUMENT_TYPE_URL,
+        creation_date=creation_date,
+        title="File part test",
+        filesize=len(content),  # in bytes
+        filename=filename,
+        content_type=content_type,
+    )
+    part = oz_document.file_parts[0]
+    # "upload" the part
+    client.proxy_file_part_upload(
+        File(BytesIO(content)),
+        file_part_uuid=part.uuid,
+        lock=oz_document.lock,
+    )
+    client.unlock_document(uuid=oz_document.uuid, lock=oz_document.lock)
+    return oz_document.uuid
 
 
 class ProcessSourceDocumentTaskTests(VCRMixin, TestCase):
@@ -50,28 +80,17 @@ class ProcessSourceDocumentTaskTests(VCRMixin, TestCase):
     def test_external_metadata_overwrites_local_metadata_and_content_is_copied(self):
         # create an actual document in the remote Open Zaak that we can point to
         with get_client(self.service) as client:
-            oz_document = client.create_document(
-                # must be unique for the source organisation
-                identification=str(uuid4()),
-                source_organisation="123456782",
-                document_type_url=DOCUMENT_TYPE_URL,
+            oz_uuid = _create_initial_document(
+                client=client,
                 creation_date=date(2025, 1, 1),
-                title="File part test",
-                filesize=10,  # in bytes
+                content=b"a" * 10,
                 filename="data.txt",
                 content_type="text/plain",
             )
-            part = oz_document.file_parts[0]
-            # "upload" the part
-            client.proxy_file_part_upload(
-                File(BytesIO(b"a" * 10)),
-                file_part_uuid=part.uuid,
-                lock=oz_document.lock,
-            )
-            client.unlock_document(uuid=oz_document.uuid, lock=oz_document.lock)
+
         source_url = (
             "http://openzaak.docker.internal:8001/documenten/api/v1/"
-            f"enkelvoudiginformatieobjecten/{oz_document.uuid}"
+            f"enkelvoudiginformatieobjecten/{oz_uuid}"
         )
         woo_document = DocumentFactory.create(
             creatiedatum=date(2000, 1, 1),
@@ -128,32 +147,18 @@ class ProcessSourceDocumentTaskTests(VCRMixin, TestCase):
     def test_copies_document_data_of_specified_version(self):
         # create a document and make multiple versions of the content
         with get_client(self.service) as client:
-            # metadata + initial upload
-            oz_document = client.create_document(
-                # must be unique for the source organisation
-                identification=str(uuid4()),
-                source_organisation="123456782",
-                document_type_url=DOCUMENT_TYPE_URL,
+            oz_uuid = _create_initial_document(
+                client=client,
                 creation_date=date(2025, 1, 1),
-                title="File part test",
-                filesize=10,  # in bytes
+                content=b"a" * 10,
                 filename="data.txt",
-                content_type="text/plain",
             )
-            part = oz_document.file_parts[0]
-            client.proxy_file_part_upload(
-                File(BytesIO(b"a" * 10)),
-                file_part_uuid=part.uuid,
-                lock=oz_document.lock,
-            )
-            client.unlock_document(uuid=oz_document.uuid, lock=oz_document.lock)
-
             # lock to create a new version
-            lock = client.post(
-                f"enkelvoudiginformatieobjecten/{oz_document.uuid}/lock"
-            ).json()["lock"]
-            patch_response = client.patch(
-                f"enkelvoudiginformatieobjecten/{oz_document.uuid}",
+            lock = client.post(f"enkelvoudiginformatieobjecten/{oz_uuid}/lock").json()[
+                "lock"
+            ]
+            client.patch(
+                f"enkelvoudiginformatieobjecten/{oz_uuid}",
                 json={
                     "inhoud": base64.b64encode(b"12345").decode("ascii"),
                     "bestandsnaam": "updated.txt",
@@ -161,12 +166,11 @@ class ProcessSourceDocumentTaskTests(VCRMixin, TestCase):
                     "lock": lock,
                 },
             )
-            patch_response.raise_for_status()
-            client.unlock_document(uuid=oz_document.uuid, lock=lock)
+            client.unlock_document(uuid=oz_uuid, lock=lock)
 
         source_url = (
             "http://openzaak.docker.internal:8001/documenten/api/v1/"
-            f"enkelvoudiginformatieobjecten/{oz_document.uuid}?versie=1"
+            f"enkelvoudiginformatieobjecten/{oz_uuid}?versie=1"
         )
         woo_document = DocumentFactory.create(source_url=source_url)
 
@@ -194,7 +198,34 @@ class ProcessSourceDocumentTaskTests(VCRMixin, TestCase):
             self.assertEqual(download.content, b"aaaaaaaaaa")
 
     def test_document_creation_is_idempotent(self):
-        raise NotImplementedError
+        with get_client(self.service) as client:
+            oz_uuid = _create_initial_document(
+                client=client,
+                creation_date=date(2025, 1, 1),
+                content=b"a" * 10,
+                filename="data.txt",
+                content_type="text/plain",
+            )
+
+        source_url = (
+            "http://openzaak.docker.internal:8001/documenten/api/v1/"
+            f"enkelvoudiginformatieobjecten/{oz_uuid}"
+        )
+        woo_document = DocumentFactory.create(source_url=source_url)
+
+        registered_document_uuids: set[str] = set()
+        for run in range(1):
+            with self.subTest(processing_run=run):
+                process_source_document(
+                    document_id=woo_document.id,
+                    base_url="http://host.docker.internal:8000/",
+                )
+
+                woo_document.refresh_from_db()
+                self.assertTrue(woo_document.upload_complete)
+                registered_document_uuids.add(str(woo_document.document_uuid))
+                # we expect the document to be registered only once
+                self.assertEqual(len(registered_document_uuids), 1)
 
     def test_file_parts_failure_is_recoverable(self):
         raise NotImplementedError
