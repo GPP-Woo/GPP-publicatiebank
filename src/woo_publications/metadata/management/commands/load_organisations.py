33535095c12aeeac042ad5257a41db1cae565740
bart-maykin <bart.burgman@maykinmedia.nl>
2025-07-04 11:35:27 +0200

    :sparkles: [#270] changed keep is_active helper func to keep is_actief and rsin

--- src/woo_publications/metadata/management/commands/load_organisations.py
+++ src/woo_publications/metadata/management/commands/load_organisations.py
@@ -1,11 +1,11 @@
 from django.core.management.commands import loaddata
 
-from ...keep_organisations_active import keep_organisations_active
+from ...keep_organisations_values import keep_organisations_values
 
 
 class Command(loaddata.Command):
     help = "Load organisations from fixture file"
 
     def handle(self, *args, **options):
-        with keep_organisations_active():
+        with keep_organisations_values():
             super().handle(*args, **options)
