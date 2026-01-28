7d0d3a9c6588026bf9516731f2c15791e653b628
Sergei Maertens <sergei@maykinmedia.nl>
2026-01-28 16:47:16 +0100

    :bug: Fix API schema documenation for UUID-filter parameters

--- src/woo_publications/api/drf_spectacular/extensions.py
+++ src/woo_publications/api/drf_spectacular/extensions.py
@@ -2,7 +2,13 @@ from inspect import isclass
 
 from drf_spectacular.contrib.django_filters import DjangoFilterExtension
 from drf_spectacular.extensions import OpenApiViewExtension
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.plumbing import (
+    build_array_type,
+    build_basic_type,
+    build_parameter_type,
+)
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework.views import APIView
 
 from woo_publications.api.utils import underscore_to_camel
@@ -11,7 +17,15 @@ from ..permissions import AuditHeaderPermission
 from .headers import ALL_AUDIT_PARAMETERS
 
 
-class CamelizeFilterExtension(DjangoFilterExtension):
+class CustomFilterExtension(DjangoFilterExtension):
+    """
+    Custom extension to address shortcomings of the default extension.
+
+    * Camelizes the filter parameters
+    * Targets the filter fields that produce warnings because the type can't be
+      auto-detected.
+    """
+
     priority = 1
 
     def get_schema_operation_parameters(
@@ -26,6 +40,40 @@ class CamelizeFilterExtension(DjangoFilterExtension):
         for parameter in parameters:
             parameter["name"] = underscore_to_camel(parameter["name"])
         return parameters
+
+    def resolve_filter_field(
+        self, auto_schema, model, filterset_class, field_name, filter_field
+    ):
+        from woo_publications.publications.api.filters import (
+            PublicationFilterSet,
+            TopicFilterSet,
+        )
+
+        match field_name:
+            case "publicaties" | "onderwerpen" if filterset_class in (
+                TopicFilterSet,
+                PublicationFilterSet,
+            ):
+                schema = build_basic_type(OpenApiTypes.UUID)
+                assert schema is not None
+                return [
+                    build_parameter_type(
+                        name=field_name,
+                        required=filter_field.extra["required"],
+                        location=OpenApiParameter.QUERY,
+                        description=self._get_field_description(filter_field, None),
+                        schema=build_array_type(schema),
+                        enum=None,
+                        explode=False,
+                        style="form",
+                    )
+                ]
+            case _:
+                pass
+
+        return super().resolve_filter_field(
+            auto_schema, model, filterset_class, field_name, filter_field
+        )
 
 
 class AuditHeadersExtension(OpenApiViewExtension):
