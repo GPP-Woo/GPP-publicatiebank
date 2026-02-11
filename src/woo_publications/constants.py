c9edf54ea1c471a996281bfc8eae33696857f3b2
bart-maykin <bart.burgman@maykinmedia.nl>
2026-02-11 10:05:12 +0100

    :fire: [#387] removed unused code

--- src/woo_publications/constants.py
+++ src/woo_publications/constants.py
@@ -11,25 +11,3 @@ class StrippableFileTypes(TextChoices):
     pdf = "pdf", _("PDF File")
     open_document = "open_document", _("Open Document")
     ms_file = "ms_files", _("Microsoft Files")
-
-
-# https://mimetype.io/all-types
-OPENDOCUMENTS = [
-    (".odc", "application/vnd.oasis.opendocument.chart"),
-    (".otc", "application/vnd.oasis.opendocument.chart-template"),
-    (".odb", "application/vnd.oasis.opendocument.database"),
-    (".odf", "application/vnd.oasis.opendocument.formula"),
-    (".odft", "application/vnd.oasis.opendocument.formula-template"),
-    (".odg", "application/vnd.oasis.opendocument.graphics"),
-    (".odgt", "application/vnd.oasis.opendocument.graphics-template"),
-    (".odi", "application/vnd.oasis.opendocument.image"),
-    (".odit", "application/vnd.oasis.opendocument.image-template"),
-    (".odp", "application/vnd.oasis.opendocument.presentation"),
-    (".odpt", "application/vnd.oasis.opendocument.presentation-template"),
-    (".ods", "application/vnd.oasis.opendocument.spreadsheet"),
-    (".odst", "application/vnd.oasis.opendocument.spreadsheet-template"),
-    (".odt", "application/vnd.oasis.opendocument.text"),
-    (".otm", "application/vnd.oasis.opendocument.text-master"),
-    (".ott", "application/vnd.oasis.opendocument.text-template"),
-    (".oth", "application/vnd.oasis.opendocument.text-web"),
-]
