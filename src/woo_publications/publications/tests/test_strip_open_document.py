dcef9ffa141596c0230a3e8a4fe70019e15ffb55
bart-maykin <bart.burgman@maykinmedia.nl>
2026-02-11 10:04:56 +0100

    :white_check_mark: [#387] added new tests for the ms document metadata strip feature

--- src/woo_publications/publications/tests/test_strip_open_document.py
+++ src/woo_publications/publications/tests/test_strip_open_document.py
@@ -3,11 +3,11 @@ import tempfile
 
 from django.test import TestCase
 
-from ..file_processing import MetaDataStripError, strip_open_document
+from ..file_processing import MetaDataStripError, strip_ms_document, strip_open_document
 
 
-class StripOpenDocumentDocumentCleanupTestCase(TestCase):
-    def test_none_zip_file(self):
+class StripMetaDataFunctionsRemoveTempFileOnErrorTests(TestCase):
+    def test_open_document_strip_error(self):
         with tempfile.NamedTemporaryFile(suffix="txt") as temp_file:
             temp_file.write(b"hello world.")
 
@@ -19,5 +19,20 @@ class StripOpenDocumentDocumentCleanupTestCase(TestCase):
                 "of the open document file",
             ):
                 strip_open_document(temp_file)
+
+            self.assertEqual(before_tmp_dir_state, os.listdir(tempfile.gettempdir()))
+
+    def test_ms_document_strip_error(self):
+        with tempfile.NamedTemporaryFile(suffix="txt") as temp_file:
+            temp_file.write(b"hello world.")
+
+            before_tmp_dir_state = os.listdir(tempfile.gettempdir())
+
+            with self.assertRaisesMessage(
+                MetaDataStripError,
+                "Something went wrong while stripping the metadata "
+                "of the MS document file",
+            ):
+                strip_ms_document(temp_file)
 
             self.assertEqual(before_tmp_dir_state, os.listdir(tempfile.gettempdir()))
