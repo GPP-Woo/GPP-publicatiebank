a9d5785752413a7f7d9ccbfcc58e2d2a062bb710
bart-maykin <bart.burgman@maykinmedia.nl>
2026-09-04 10:36:00 +0200

    :rotating_light: [#468] ignore linting rules and apply ruff

--- src/woo_publications/gunicorn_files.py
+++ src/woo_publications/gunicorn_files.py
@@ -1,11 +1,45 @@
 import json
-import logging
+import logging  # noqa: TID251
 from datetime import datetime
 
 from gunicorn.glogging import Logger
 
-class JsonFormatter(logging.Formatter):
-    def format(self, record):
+
+class AccessLogFormatter(logging.Formatter):
+    def format(self, record) -> str:
+        args = record.args
+
+        if not isinstance(args, dict):
+            return ""
+
+        assert isinstance(args["t"], str)
+        dt = datetime.strptime(args["t"], "[%d/%b/%Y:%H:%M:%S %z]")
+
+        payload = {
+            "source": "gunicorn",
+            "event": "request",
+            "level": record.levelname,
+            "logger": record.name,
+            "process": record.process,
+            "thread": record.thread,
+            "method": args["m"],
+            "path": args["U"],
+            "duration_in_ms": args["M"],
+            "status": args["s"],
+            "bytes": args["b"],
+            "query": args["q"],
+            "referer": args["{referer}i"],
+            "host": args["{host}i"],
+            "timestamp": dt.strftime("%Y-%m-%dT%H:%M:%S%z"),
+            "remote_ip": "{http_x_forwarded_for}e",
+            "message": record.getMessage(),
+        }
+
+        return json.dumps(payload, default=str)
+
+
+class ErrorLogFormatter(logging.Formatter):
+    def format(self, record) -> str:
         payload = {
             "source": "gunicorn",
             "type": "server",
@@ -13,31 +47,10 @@ class JsonFormatter(logging.Formatter):
             "logger": record.name,
             "process": record.process,
             "thread": record.thread,
+            "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z"),
+            "message": record.getMessage(),
         }
 
-        if record.name != "gunicorn.access":
-            payload["message"] = record.getMessage()
-
-        if isinstance(record.args, dict):
-            args = record.args
-            dt = datetime.strptime(
-                args["t"], "[%d/%b/%Y:%H:%M:%S %z]"
-            )
-
-            payload |= {
-                "event": "request",
-                "status": int(args["s"]),
-                "method": args["m"],
-                "path": args["U"],
-                "query": args["q"],
-                "bytes": args["b"],
-                "host": args["h"],
-                "timestamp": dt.strftime("%Y-%m-%dT%H:%M:%S%z"),
-                "duration_ms": args["M"],
-            }
-
-        if record.exc_info:
-            payload["exception"] = self.formatException(record.exc_info)
         return json.dumps(payload, default=str)
 
 
@@ -45,10 +58,8 @@ class JsonGunicornLogger(Logger):
     def setup(self, cfg):
         super().setup(cfg)
 
-        json_formatter = JsonFormatter()
-
         for handler in self.access_log.handlers:
-            handler.setFormatter(json_formatter)
+            handler.setFormatter(AccessLogFormatter())
 
         for handler in self.error_log.handlers:
-            handler.setFormatter(json_formatter)
+            handler.setFormatter(ErrorLogFormatter())
--- src/woo_publications/wsgi.py
+++ src/woo_publications/wsgi.py
@@ -7,8 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-from datetime import UTC, datetime
-
 from django.core.wsgi import get_wsgi_application
 
 from woo_publications.setup import setup_env
