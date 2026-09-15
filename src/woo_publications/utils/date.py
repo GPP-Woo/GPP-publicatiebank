98b2ddb0300d736fc57170eb92e9dbd30b35d5c1
bart-maykin <bart.burgman@maykinmedia.nl>
2026-09-15 16:11:27 +0200

    :memo: [#458] added doc string to get_workday function

--- src/woo_publications/utils/date.py
+++ src/woo_publications/utils/date.py
@@ -4,6 +4,14 @@ from workalendar.europe import Netherlands as DutchCalendar
 
 
 def get_workday(date: datetime.date) -> datetime.date:
+    """
+    Get the first available workday for the given date.
+    If the given date is on a workday it simply returns it,
+    if it falls on a holiday or in the weekend then it checks and returns the next
+    available workday.
+
+    :arg date: A date object which we should base the first available workday on.
+    """
     calendar = DutchCalendar()
 
     # inefficient way to check for next work day but since the
